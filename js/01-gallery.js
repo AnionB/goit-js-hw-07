6b517bde5df2ba64e0f339db29e48590115a8efc
AnionB <anion0111@gmail.com>
2022-01-20 18:52:17 +0200

    1 и 2 задача дз

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+createGallery();
+
+function createGallery() {
+  const imgsHTML = galleryItems
+    .map(
+      ({ preview, description, original }) =>
+        ` <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`,
+    )
+    .join('');
+
+  galleryEl.insertAdjacentHTML('afterbegin', imgsHTML);
+}
+galleryEl.addEventListener('click', onImgClick);
+function onImgClick(evt) {
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  evt.preventDefault();
+
+  openModalImg(evt);
+
+  return;
+}
+function openModalImg(e) {
+  const opt = {
+    onShow: () => document.addEventListener('keydown', onEscapePress),
+    onClose: () => document.removeEventListener('keydown', onEscapePress),
+  };
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="800" height="600">`,
+    opt,
+  );
+
+  instance.show();
+
+  function onEscapePress(evt) {
+    if (evt.key !== 'Escape') {
+      return;
+    }
+    instance.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+createGallery();
+createModalImg();
 
-console.log(galleryItems);
+function createGallery() {
+  const imgsHTML = galleryItems
+    .map(
+      ({ preview, description, original }) =>
+        ` <li>
+        <a class="gallery__item" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"           
+          />
+        </a>
+      </li> `,
+    )
+    .join('');
+
+  galleryEl.insertAdjacentHTML('afterbegin', imgsHTML);
+}
+function createModalImg() {
+  const opt = {
+    captionsData: 'alt',
+    captionDelay: 250,
+  };
+  const lightbox = new SimpleLightbox('.gallery a', opt);
+}
